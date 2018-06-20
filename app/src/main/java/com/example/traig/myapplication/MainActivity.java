a94c93f63b212fb0f89a3f4cf5c5c0b6a0bf6cf2
DESKTOP-2PFPM2E\TranThai <thaitran101096@gmail.com>
2018-06-20 14:30:02 +0700

    add click listener

--- app/src/main/java/com/example/traig/myapplication/MainActivity.java
+++ app/src/main/java/com/example/traig/myapplication/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.traig.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button btnClick;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //edit
+
+        btnClick=findViewById(R.id.btnClick);
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,"Notification",Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
